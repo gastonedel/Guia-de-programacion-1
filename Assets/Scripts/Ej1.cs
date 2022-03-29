7657b06a51d5764104d7a5088f8e563639818298
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-03-29 10:40:38 -0300

    ex 1 and 2

--- Assets/Scripts/Ej1.cs
+++ Assets/Scripts/Ej1.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ej1 : MonoBehaviour
+    
 {
     // Start is called before the first frame update
+    /*1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+    valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el
+    resultado de multiplicarlo por sí mismo.*/
+   public int num1;
     void Start()
+
+        
     {
         
+        num1 = 0;
+        num1 = 2;
+        Debug.Log(num1);
+        num1 = num1 * num1;
+        Debug.Log(num1);
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
+respectivamente, realice la operación correspondiente y muestre el resultado en pantalla:
+a. Suma
+b. Producto*/
+
+    void Start()
+    {
+        int num1;
+        int num2;
+        num1 = 4;
+        num2 = 5;
+       int suma= num1 + num2;
+        Debug.Log(suma);
+        int producto = num1 * num2;
+        Debug.Log(producto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
